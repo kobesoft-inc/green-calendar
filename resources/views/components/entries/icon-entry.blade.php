87a108ead0d42e7227cf33a5e1c7c9c3b3a45a9c
松岡 利昌 <t.matsuoka@kobesoft.co.jp>
2024-04-17 15:17:30 +0900

    Update icon-entry.blade.php

--- resources/views/components/entries/icon-entry.blade.php
+++ resources/views/components/entries/icon-entry.blade.php
@@ -10,19 +10,21 @@
             ->class(['gc-icon-entry'])
     }}
 >
-    <x-filament::icon
-        :icon="$icon"
-        @class([
-            match ($color) {
-                'gray' => 'text-gray-400 dark:text-gray-500',
-                default => 'text-custom-500 dark:text-custom-400',
-            },
-        ])
-        @style([
-            \Filament\Support\get_color_css_variables(
-                $color,
-                shades: [400, 500],
-            ) => $color !== 'gray',
-        ])
-    />
+    <div>
+        <x-filament::icon
+            :icon="$icon"
+            @class([
+                match ($color) {
+                    'gray' => 'text-gray-400 dark:text-gray-500',
+                    default => 'text-custom-500 dark:text-custom-400',
+                },
+            ])
+            @style([
+                \Filament\Support\get_color_css_variables(
+                    $color,
+                    shades: [400, 500],
+                ) => $color !== 'gray',
+            ])
+        />
+    </div>
 </div>
